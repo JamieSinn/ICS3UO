4053eb9ea6b534ed5540327c788afeb34f25a7d1
BuildTools <unconfigured@null.spigotmc.org>
2015-02-05 08:36:32 -0500

    Basics Project

---
+++ basics/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Jamie Sinn - ICS3UO</title>
+
+    <meta name="description" content="The basics of creating an HTML5 page">
+    <meta name="author" content="HTML Basics">
+
+    <link rel="stylesheet" href="styles.css?v=1.0">
+</head>
+<body>
+
+    <h1>Non Descriptive H1 Title</h1>
+
+    <h2>Boring H2 title</h2>
+
+    <h3>Boring H3 title</h3>
+    <h4 class="fancy">Fancy H4 Title</h4>
+    
+    <p class="fancy">content of page goes <b>in the body.</b></p>
+    <p>content of <span class="blue-text">page</span> goes in the body.  This paragraph has no style. :)</p>
+    <p class="fancy">This is a link to <a href="http://www.google.ca">Google</a></p>
+    <p class="fancy">
+    This is an image of a spaceship<br />
+    <img src="http://ramrobazaar.com/uploads/1341596825343s1jpg.jpg" title="Spaceship" class="rounded-corner" />
+    <br /><br />
+    The following text was generated from: <a href="http://www.lipsum.com/" class="cool">http://www.lipsum.com/</a><br />
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et quam diam. Praesent dolor sem, pulvinar eget fermentum at, ultrices in eros. Proin scelerisque tellus dapibus arcu ultricies iaculis. Vivamus sodales dolor a ligula suscipit eget viverra mauris porta. Fusce dictum metus id ipsum sodales consectetur. Suspendisse nibh odio, ultricies ac vulputate a, aliquet mollis metus. Curabitur ultricies pretium lorem a hendrerit. Sed malesuada vulputate eros vel molestie. Integer nulla felis, pharetra ac dignissim id, fringilla quis nunc. Nam sit amet massa eget nibh placerat commodo at vitae felis. Duis mattis neque eget dui euismod in fringilla erat fermentum. Proin faucibus lobortis pellentesque. Nullam ligula tortor, mattis sed eleifend vitae, consequat tincidunt libero. Ut a mollis mauris.
+    </p>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</body>
+</html>
