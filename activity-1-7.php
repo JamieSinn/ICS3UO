ae1af5974011df7d6bb93e357b0942b3775de9e2
JamieSinn <james.sinn@sinndevelopment.com>
2015-02-18 09:33:20 -0500

    Close #6

---
+++ activity-1-7.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Activity 1.7 - Embedding with iFrames</title>
+    <meta name="description" content="More about CSS">
+    <meta name="author" content="Jamie Sinn">
+    <link rel="stylesheet" href="styles.css?v=1.0">
+
+</head>
+<body>
+<!-- main content -->
+
+<!--YouTube-->
+<h2>YouTube embed with iframes</h2>
+
+<iframe width="640" height="360" src="https://www.youtube.com/embed/XVjeOTKeAjw?feature=player_embedded" frameborder="0" allowfullscreen align="center">
+</iframe>
+<p>Current Favourite Song</p>
+
+<!--Google Maps-->
+<h2>Embedding Google Maps</h2>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83328.80618138779!2d-123.12391349999999!3d49.256968400000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673f143a94fb3%3A0xbb9196ea9b81f38b!2sVancouver%2C+BC!5e0!3m2!1sen!2sca!4v1424269873892" width="600" height="450" frameborder="0" style="border:0">
+</iframe>
+<p>I grew up in North Vancouver, and I love going back to Vancouver every summer.</p>
+<!-- /main content -->
+
+<!-- load jquery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</body>
+</html>
+
