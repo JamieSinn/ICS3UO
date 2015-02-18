6aaca391e108bd9747a6a86e6fbc67cfa01410c8
JamieSinn <james.sinn@sinndevelopment.com>
2015-02-18 09:34:51 -0500

    Add line breaks to index page

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
             <a href="activity-1-1.php">Activity 1.1</a><br/>
             <a href="activity-1-2.php">Activity 1.2</a><br/>
             <a href="activity-1-3.php">Activity 1.3</a><br/>
-            <a href="activity-1-4.php">Activity 1.4 (Big Mac Table Activity)</a>
-            <a href="activity-1-5.php">Activity 1.5</a>
+            <a href="activity-1-4.php">Activity 1.4 (Big Mac Table Activity)</a> <br/>
+            <a href="activity-1-5.php">Activity 1.5</a> <br/>
             <!-- Apparently 5 + 1 = 7-->
-            <a href="activity-1-7.php">Activity 1.7</a>
+            <a href="activity-1-7.php">Activity 1.7</a><br/>
     </p>
 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
